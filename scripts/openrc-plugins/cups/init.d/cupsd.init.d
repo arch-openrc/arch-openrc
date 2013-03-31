c035b15f424585ab2425a1be340a5577dc247727
udeved <udeved@openrc4arch.site40.net>
2013-03-31 20:18:06 +0200

    fix

--- scripts/openrc-plugins/cups/init.d/cupsd.init.d
+++ scripts/openrc-plugins/cups/init.d/cupsd.init.d
@@ -1,7 +1,13 @@
 #!/usr/sbin/runscript
 
 depend() {
-	use net dbus avahi-daemon dbus
+	use net dbus avahi-daemon
+	before nfs
+	after logger
+}
+
+start() {
+	ebegin "Starting cupsd"
 	checkpath -q -d -m 0755 -o root:lp /run/cups
 	checkpath -q -d -m 0511 -o daemon:sys /run/cups/certs
 	start-stop-daemon --start --quiet --exec /usr/sbin/cupsd
