e2d7a9b36a72369909ba7e31b8dc98b2759d5ebd
artoo <flower_of_life@gmx.net>
2014-06-23 03:35:53 +0200

    update build.sh

--- build-for-manjaro.sh
+++ build-for-manjaro.sh
@@ -10,21 +10,21 @@ if [ "$EUID" != "0" ]; then
     exit 1
 fi
 
-user=$(ls /var/lib/manjarobuild/unstable-`uname -m` | cut -d' ' -f1 | grep -v root | grep -v lock)
+user=$(ls /var/lib/manjarobuild/unstable-$(uname -m) | cut -d' ' -f1 | grep -v root | grep -v lock)
 
 echo "Build eudev"
 date
 cd eudev/libsystemd-eudevcompat
+### boxit ???
+unstable-$(uname -m)-build -c
 
-unstable-`uname -m`-build -c
-
-makechrootpkg -n -r /var/lib/manjarobuild/unstable-`uname -m`
-pacman -Rdd libsystemd -r /var/lib/manjarobuild/unstable-`uname -m`/$user --noconfirm
-pacman -U *`uname -m`*pkg*z -r /var/lib/manjarobuild/unstable-`uname -m`/$user --noconfirm
+makechrootpkg -n -r /var/lib/manjarobuild/unstable-$(uname -m)
+pacman -Rdd libsystemd -r /var/lib/manjarobuild/unstable-$(uname -m)/$user --noconfirm
+pacman -U *$(uname -m)*pkg*z -r /var/lib/manjarobuild/unstable-$(uname -m)/$user --noconfirm
 
 cd ../eudev
-makechrootpkg -n -r /var/lib/manjarobuild/unstable-`uname -m`
-pacman -U *`uname -m`*pkg*z -r /var/lib/manjarobuild/unstable-`uname -m`/$user --noconfirm
+makechrootpkg -n -r /var/lib/manjarobuild/unstable-$(uname -m)
+pacman -U *$(uname -m)*pkg*z -r /var/lib/manjarobuild/unstable-$(uname -m)/$user --noconfirm
 echo "Done building eudev"
 
 echo "Build openrc"
@@ -32,7 +32,7 @@ date
 cd ../../openrc
 for pkg in $(cat build-list); do
 	cd $pkg
-	makechrootpkg -n -r /var/lib/manjarobuild/unstable-`uname -m` || break && cd ..;
+	makechrootpkg -n -r /var/lib/manjarobuild/unstable-$(uname -m) || break && cd ..
 done
 date
 echo "Done building openrc"
@@ -40,7 +40,10 @@ echo "Done building openrc"
 echo "Build eudev (additional packages)"
 date
 cd ../eudev
-for pkg in $(cat build-list); do cd $pkg && makechrootpkg -n -r /var/lib/manjarobuild/unstable-`uname -m` || break && cd ..; done
+for pkg in $(cat build-list); do
+	cd $pkg
+	makechrootpkg -n -r /var/lib/manjarobuild/unstable-$(uname -m) || break && cd ..
+done
 date
 echo "Done building eudev (additional packages)"
 #shutdown -h now
