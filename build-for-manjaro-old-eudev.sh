add07093738721976ffd206b87d27072aedad594
artoo <flower_of_life@gmx.net>
2014-06-23 03:05:56 +0200

    minor changes

--- build-for-manjaro-old-eudev.sh
+++ build-for-manjaro-old-eudev.sh
@@ -13,7 +13,7 @@ fi
 user=$(ls /var/lib/manjarobuild/unstable-`uname -m` | cut -d' ' -f1 | grep -v root | grep -v lock)
 echo "Build eudev"
 date
-cd old_eudev/dbus-eudev
+cd unused/old_eudev/dbus-eudev
 unstable-`uname -m`-build -c
 cd ../eudev
 makechrootpkg -n -r /var/lib/manjarobuild/unstable-`uname -m`
@@ -22,13 +22,13 @@ pacman -U *`uname -m`*pkg*z -r /var/lib/manjarobuild/unstable-`uname -m`/$user -
 echo "Done building eudev"
 echo "Build openrc"
 date
-cd ../../openrc
+cd ../../../openrc
 for pkg in $(cat build-list); do cd $pkg && makechrootpkg -n -r /var/lib/manjarobuild/unstable-`uname -m` || break && cd ..; done
 date
 echo "Done building openrc"
 echo "Build eudev (additional packages)"
 date
-cd ../old_eudev
+cd ../../../unused/old_eudev
 for pkg in $(cat build-list); do cd $pkg && makechrootpkg -n -r /var/lib/manjarobuild/unstable-`uname -m` || break && cd ..; done
 date
 echo "Done building eudev (additional packages)"
