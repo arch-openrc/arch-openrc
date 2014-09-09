a5a5db01097292fc4ebc09841f7fe309e646959d
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2014-09-10 01:09:35 +0530

    Move package signing to msign.sh

--- mbuild.sh
+++ mbuild.sh
@@ -10,8 +10,7 @@ export LC_MESSAGES=C
 # Command line arguments
 BRANCH=${1:-unstable}	# Branch to build for, defaulting to unstable
 ARCH=${2:-$(uname -m)}	# Arch to build for, defaulting to system's arch
-SIGN=${3:-0}  		# Whether to sign packages or not. 1 = sign
-IS_EXTRA_EUDEV=${4:-0}  # since upower-0.9.23 is deprecated in upstream, it isn't expected to change
+IS_EXTRA_EUDEV=${3:-0}  # since upower-0.9.23 is deprecated in upstream, it isn't expected to change
 
 # Parse config file for mbuild
 if [[ -f ./mbuild.conf ]];then
@@ -29,24 +28,26 @@ fi
 # Keep track of the base build directory
 cwd=`pwd`
 
-echo "==> Start building eudev"
-date
-#cd eudev/eudev-systemdcompat
+# Dont know what this does
 user=$(ls ${CHROOT}/${BRANCH}-${ARCH} | cut -d' ' -f1 | grep -v root | grep -v lock)
 
+# Setup chroot
 ${BRANCH}-${ARCH}-build -c -r ${CHROOT}
 #${BRANCH}-${ARCH}-build -r ${CHROOT}  # -c (Dont clean chroot everytime)
-cd eudev
 
+#cd eudev/eudev-systemdcompat
+echo "==> Start building eudev"
+date
+cd eudev
 for pkg in $(cat build-list); do cd $pkg && makechrootpkg -n -r ${CHROOT}/${BRANCH}-${ARCH} || break && cd ..; done
 date
+echo "==> Done building eudev"
 #makechrootpkg -n -r ${CHROOT}/${BRANCH}-${ARCH}
 #pacman -Rdd libsystemd -r ${CHROOT}/${BRANCH}-${ARCH}/$user --noconfirm
 #pacman -U *${ARCH}*pkg*z -r ${CHROOT}/${BRANCH}-${ARCH}/$user --noconfirm
 #cd ../eudev
 #makechrootpkg -n -r ${CHROOT}/${BRANCH}-${ARCH}
 #pacman -U *${ARCH}*pkg*z -r ${CHROOT}/${BRANCH}-${ARCH}/$user --noconfirm
-echo "==> Done building eudev"
 
 echo "==> Start building openrc"
 date
@@ -64,17 +65,7 @@ if (( $IS_EXTRA_EUDEV ));then
 	echo "==> Done building upower-pm-utils"
 fi
 
-### Sign packages
-if [ "$SIGN" -eq 1 ]; then
-	## eudev
-	cd $cwd # Back to the base directory
-	cd eudev
-	for pkg in $(cat build-list); do cd $pkg && signpkgs || break && cd ..; done
-	
-	## openrc
-	cd $cwd # Back to the base directory
-	cd openrc
-	for pkg in $(cat build-list); do cd $pkg && signpkgs || break && cd ..; done
-fi
+## Sign packages
+# Moved to the msign.sh script
 
 #shutdown -h now
