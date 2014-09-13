3880c72cf8b2111d31567e8ab03cbff742112f46
artoo <flower_of_life@gmx.net>
2014-09-13 13:53:13 +0200

    update pkgbuilds

--- build/prebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-## prebuild.sh: pre install steps before building openrc and eudev packages
-
-# Keep track of the base directory
-cwd=`pwd`
-
-cd ..
-git clean -dfx  # Remove untracked directories and files
