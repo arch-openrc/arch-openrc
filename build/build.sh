cb9a6ae061dad119c7ed26828044e96763698d51
udeved <flower_of_life@gmx.net>
2014-09-27 00:34:03 +0200

    update build.sh

--- build/build.sh
+++ build/build.sh
@@ -26,52 +26,45 @@ chroot_build(){
 }
 
 mk_pkg(){
+	run_pre
 
-    #if [ "$EUID" == "0" ]; then
-
-      case ${START_DIR} in
-	      openrc)
-		START_PKG=sysvinit
-		chroot_init
-		chroot_build
-	      ;;
-	      eudev)
-		START_PKG=eudev
-		chroot_init
-		chroot_build
-	      ;;
-	      * | all)
-		START_PKG=sysvinit
-		START_DIR=openrc
-		chroot_init
-		chroot_build
-		START_PKG=eudev
-		START_DIR=eudev
-		chroot_init
-		chroot_build
-	      ;;
-      esac
-
-    #else
-      #echo "error: This script must be run as root."
-      #exit 1
-    #fi
+	case ${START_DIR} in
+		openrc)
+		  START_PKG=sysvinit
+		  chroot_init
+		  chroot_build
+		  mk_sign
+		  run_post
+		;;
+		eudev)
+		  START_PKG=eudev
+		  chroot_init
+		  chroot_build
+		  mk_sign
+		  run_post
+		;;
+		* | all)
+		  START_PKG=sysvinit
+		  START_DIR=openrc
+		  chroot_init
+		  chroot_build
+		  mk_sign
+		  run_post
+		  START_PKG=eudev
+		  START_DIR=eudev
+		  chroot_init
+		  chroot_build
+		  mk_sign
+		  run_post
+		;;
+	esac
 }
 
 mk_sign(){
 
-    # openrc
-    cd ${RUN_DIR} && cd ../openrc/${START_PKG}
+    cd ${RUN_DIR} && cd ../${START_DIR}/${START_PKG}
     signpkgs
     cd ..
-    for pkg in $(cat build-list); do
-      cd $pkg
-      signpkgs
-      cd ..
-    done
-
-    # eudev
-    cd ${RUN_DIR} && cd ../eudev
     for pkg in $(cat build-list); do
       cd $pkg
       signpkgs
@@ -87,18 +80,9 @@ run_post(){
       mkdir -p "$pkgdir"
     fi
 
-    # openrc
-    cd ${RUN_DIR} && cd ../openrc/${START_PKG}
+    cd ${RUN_DIR} && cd ../${START_DIR}/${START_PKG}
     cp -v *.pkg.tar.xz{,.sig} $pkgdir
     cd ..
-    for pkg in $(cat build-list); do
-      cd $pkg
-      cp -v *.pkg.tar.xz{,.sig} $pkgdir
-      cd ..
-    done
-
-    # eudev
-    cd ${RUN_DIR} && cd ../eudev
     for pkg in $(cat build-list); do
       cd $pkg
       cp -v *.pkg.tar.xz{,.sig} $pkgdir
@@ -126,12 +110,6 @@ ARCH=${2:-$(uname -m)}
 START_DIR=${3:-all} # options: openrc,eudev,all
 USR=$(ls ${CHROOT}/${BRANCH}-${ARCH} | cut -d' ' -f1 | grep -v root | grep -v lock)
 
-run_pre
-
 mk_pkg $1 $2 $3
-
-mk_sign
-
-run_post
 
 #shutdown -h now
