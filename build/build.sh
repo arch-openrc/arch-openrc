697124db05eb6dc266e8d1b1372768d9a6303cfe
udeved <flower_of_life@gmx.net>
2014-09-27 00:02:05 +0200

    update build script

--- build/build.sh
+++ build/build.sh
@@ -6,46 +6,51 @@ run_pre(){
     git clean -dfx  # Remove untracked directories and files
 }
 
-# $1: branch
-# $2: arch
+chroot_init(){
+    local blacklist=('libsystemd')
+    cd ${RUN_DIR} && cd ../${START_DIR}/${START_PKG}
+    echo "==> building ${START_PKG}"
+    sudo ${BRANCH}-${ARCH}-build -c -r ${CHROOT}
+    sudo pacman -Rdd ${blacklist[@]} -r ${CHROOT}/${BRANCH}-${ARCH}/$USR --noconfirm
+    sudo pacman -U *${ARCH}*pkg*z -r ${CHROOT}/${BRANCH}-${ARCH}/$USR --noconfirm
+    cd ..
+}
+
+chroot_build(){
+      for pkg in $(cat build-list); do
+	cd $pkg
+	echo "==> building $pkg"
+	sudo makechrootpkg -n -r ${CHROOT}/${BRANCH}-${ARCH} || break
+	cd ..
+      done
+}
+
 mk_pkg(){
 
     #if [ "$EUID" == "0" ]; then
 
-      local branch=${1:-unstable}
-      local arch=${2:-$(uname -m)}
-      local user=$(ls ${CHROOT}/${branch}-${arch} | cut -d' ' -f1 | grep -v root | grep -v lock)
-      # to be removed for eudev builds
-      local blacklist='libsystemd systemd'
-
-      # openrc
-      date
-      cd ${RUN_DIR} && cd ../openrc/${START_PKG}
-      echo "==> building ${START_PKG}"
-      sudo ${branch}-${arch}-build -c -r ${CHROOT}
-      cd ..
-      for pkg in $(cat build-list); do
-	cd $pkg
-	echo "==> building $pkg"
-	sudo makechrootpkg -n -r ${CHROOT}/${branch}-${arch} || break
-	cd ..
-      done
-      date
-
-      # eudev
-      date
-      cd ${RUN_DIR} && cd ../eudev
-      for pkg in $(cat build-list); do
-	cd $pkg
-	echo "==> building $pkg"
-	sudo makechrootpkg -n -r ${CHROOT}/${branch}-${arch} || break
-	if [[ $pkg == "eudev" ]];then
-	  sudo pacman -Rdd ${blacklist[@]} -r ${CHROOT}/${branch}-${arch}/$user --noconfirm
-	  sudo pacman -U *${arch}*pkg*z -r ${CHROOT}/${branch}-${arch}/$user --noconfirm
-	fi
-	cd ..
-      done
-      date
+      case ${START_DIR} in
+	      openrc)
+		START_PKG=sysvinit
+		chroot_init
+		chroot_build
+	      ;;
+	      eudev)
+		START_PKG=eudev
+		chroot_init
+		chroot_build
+	      ;;
+	      * | all)
+		START_PKG=sysvinit
+		START_DIR=openrc
+		chroot_init
+		chroot_build
+		START_PKG=eudev
+		START_DIR=eudev
+		chroot_init
+		chroot_build
+	      ;;
+      esac
 
     #else
       #echo "error: This script must be run as root."
@@ -105,8 +110,6 @@ run_post(){
 
 ##############MAIN###################
 
-#set -x
-
 export LANG=C
 export LC_MESSAGES=C
 
@@ -116,17 +119,19 @@ if [[ -f ./build.conf ]];then
     . "./build.conf"
 else
     CHROOT=/opt/manjarobuild
-    START_PKG=sysvinit
 fi
+
+BRANCH=${1:-unstable}
+ARCH=${2:-$(uname -m)}
+START_DIR=${3:-all} # options: openrc,eudev,all
+USR=$(ls ${CHROOT}/${BRANCH}-${ARCH} | cut -d' ' -f1 | grep -v root | grep -v lock)
 
 run_pre
 
-mk_pkg $1 $2
+mk_pkg $1 $2 $3
 
 mk_sign
 
 run_post
-
-
 
 #shutdown -h now
