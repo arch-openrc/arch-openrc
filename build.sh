43bb97f1e71a5dd1e509d16d26b355e4afe710d1
artoo <flower_of_life@gmx.net>
2014-09-29 12:47:43 +0200

    add profiles

--- build.sh
+++ build.sh
@@ -1,77 +1,118 @@
 #!/bin/bash
 ## build.sh: all the steps for building openrc and/or eudev packages
 
-run_clean(){
-	cd ..
-	git clean -dfx  # Remove untracked directories and files
+git_clean(){
+    git clean -dfx  # Remove untracked directories and files
 }
 
 get_user(){
-	echo $(ls ${CHROOT}/${BRANCH}-${ARCH} | cut -d' ' -f1 | grep -v root | grep -v lock)
+    echo $(ls ${WORKDIR} | cut -d' ' -f1 | grep -v root | grep -v lock)
 }
 
-rm_blacklisted(){
-	local blacklist=('libsystemd')
-	sudo pacman -Rdd ${blacklist[@]} -r ${CHROOT}/${BRANCH}-${ARCH}/$(get_user) --noconfirm
+rm_bl_pkg(){
+    local blacklist=('libsystemd')
+    sudo pacman -Rdd ${blacklist[@]} -r ${WORKDIR}/$(get_user) --noconfirm
 }
 
-install_built(){
-	sudo pacman -U *${ARCH}*pkg*z -r ${CHROOT}/${BRANCH}-${ARCH}/$(get_user) --noconfirm
+inst_pkg(){
+    sudo pacman -U *${ARCH}*pkg*z -r ${WORKDIR}/$(get_user) --noconfirm
 }
 
 chroot_init(){
-	cd ${RUN_DIR} && cd ../${START_DIR}/${START_PKG} && echo "==> building ${START_PKG}"
-	sudo "${BRANCH}-${ARCH}-build" -c -r ${CHROOT}
-	rm_blacklisted && install_built
-	cd ..
+    cd $1 && echo "==> Start building $1"
+    sudo ${BRANCH}-${ARCH}-build ${NOCLEAN} -r ${CHROOT} #-- ${NOVERIFY} ${NOCLEAN}
+    rm_bl_pkg && inst_pkg
+    echo "==> End building $1" && cd ..
 }
 
+# $1: name
 chroot_pkg(){
-	for pkg in $(cat build-list); do
-		cd $pkg && echo "==> building $pkg"
-		sudo makechrootpkg -n -b ${BRANCH} -r ${CHROOT}/${BRANCH}-${ARCH} || break
-		cd ..
-	done
-}
-
-# $1: task
-run_task(){
-
-	cd ${RUN_DIR} && cd ../${START_DIR}/${START_PKG} && $1 && cd ..
-	for pkg in $(cat build-list); do
-		cd $pkg && $1 && cd ..
-	done
+    for pkg in $(cat ${PROFDIR}/$1); do
+	cd $pkg && echo "==> Start building $pkg"
+	#sudo makechrootpkg ${NOVERIFY} -b ${BRANCH} -r ${WORKDIR} -- ${NOCLEAN} || break
+	sudo makechrootpkg ${NOVERIFY} -b ${BRANCH} -r ${WORKDIR} || break
+	echo "==> End building $pkg" && cd ..
+    done
 }
 
 cp_pkg(){
-	if [ ! -d "${RUN_DIR}/packages/" ]; then
-		mkdir -p "${RUN_DIR}/packages/"
-	fi
-	cp *.pkg.tar.xz{,.sig} ${RUN_DIR}/packages/
+    if [[ ! -d "${PKGDIR}" ]]; then
+	mkdir -p ${PKGDIR}
+    fi
+
+    case $1 in
+	all)
+	    cd eudev
+	    cp *.pkg.tar.xz ${PKGDIR}
+	    cd ..
+	    for pkg in $(cat ${PROFDIR}/$1); do
+	    cd $pkg
+		    cp *.pkg.tar.xz ${PKGDIR}
+	    cd ..
+	    done
+	;;
+	sysvinit | eudev)
+	    cd $1
+	    cp *.pkg.tar.xz ${PKGDIR}
+	    cd ..
+	    for pkg in $(cat ${PROFDIR}/$1); do
+	    cd $pkg
+		    cp *.pkg.tar.xz ${PKGDIR}
+	    cd ..
+	    done
+	;;
+	*) break ;;
+    esac
 }
 
-mk_pkg(){
-	run_clean
-	case ${START_DIR} in
-		openrc)
-			START_PKG=sysvinit
-			chroot_init && chroot_pkg
-			run_task signpkgs && run_task cp_pkg
-		;;
-		eudev)
-			START_PKG=eudev
-			chroot_init && chroot_pkg
-			run_task signpkgs && run_task cp_pkg
-		;;
-		* | all)
-			START_PKG=sysvinit && START_DIR=openrc
-			chroot_init && chroot_pkg
-			mk_sign && run_post
-			START_PKG=eudev && START_DIR=${START_PKG}
-			chroot_init && chroot_pkg
-			run_task signpkgs && run_task cp_pkg
-		;;
-	esac
+sign_pkg(){
+	cd ${PKGDIR} &&	signpkgs && cd ..
+}
+
+run(){
+    git_clean
+
+    case ${PROFILE} in
+	sysvinit)
+	    echo "==> Start building profile ${PROFILE}"
+	    chroot_init "${PROFILE}"
+	    chroot_pkg "${PROFILE}"
+	    echo "==> Finished building profile ${PROFILE}"
+	    cp_pkg "${PROFILE}"
+	    if (( ${SIGN} ));then
+		    sign_pkg
+	    fi
+	;;
+	eudev)
+	    echo "==> Start building profile ${PROFILE}"
+	    chroot_init "${PROFILE}"
+	    chroot_pkg "${PROFILE}"
+	    echo "==> Finished building profile ${PROFILE}"
+	    cp_pkg  "${PROFILE}"
+	    if (( ${SIGN} ));then
+		    sign_pkg
+	    fi
+	;;
+	all)
+	    echo "==> Start building profile ${PROFILE}"
+	    chroot_init "eudev"
+	    chroot_pkg "${PROFILE}"
+	    echo "==> Finished building profile ${PROFILE}"
+	    cp_pkg  "all"
+	    if (( ${SIGN} ));then
+		    sign_pkg
+	    fi
+	;;
+	*)
+	    echo "==> Start building package ${PROFILE}"
+	    chroot_init "${PROFILE}"
+	    echo "==> Finished building package ${PROFILE}"
+	    cp_pkg  "${PROFILE}"
+	    if (( ${SIGN} ));then
+		    sign_pkg
+	    fi
+	;;
+    esac
 }
 
 ########################MAIN################################
@@ -79,43 +120,44 @@ mk_pkg(){
 export LANG=C
 export LC_MESSAGES=C
 
-RUN_DIR=$(pwd)
-
-BRANCH='unstable'
+BRANCH=unstable
 ARCH=$(uname -m)
-START_DIR='all' # all,eudev,openrc
-CHROOT='/srv/manjarobuild'
+PROFILE=all # all,eudev,sysvinit, or single package
+CHROOT=/srv/manjarobuild
+SIGN=0
+NOCLEAN='-c'
+UPDATE=''
+NOVERIFY='-n'
+RUNDIR=$(pwd)
+PKGDIR=${RUNDIR}/packages/
+PROFDIR=${RUNDIR}/profiles
 
 usage() {
-	echo 'Usage: build.sh [options]'
-	echo ' options:'
-	echo '    -b <branch>   [default] '"${BRANCH}"
-	echo '    -a <arch>     [default] '"${ARCH}"
-	echo '    -s <startdir> [default] '"${START_DIR}"
-	echo '    -r <chroot>   [default] '"${CHROOT}"
-	echo '    -h            Help'
-	exit 1
+    echo 'Usage: build.sh [options]'
+    echo ' options:'
+    echo '    -b <branch>         [default] '"${BRANCH}"
+    echo '    -a <arch>           [default] '"${ARCH}"
+    echo '    -p <profile>        [default] '"${PROFILE}"
+    echo '    -s                  sign packages'
+    echo '    -n                  no clean chroot'
+    echo '    -x                  no namcap checks'
+    echo '    -h                  Help'
+    exit 1
 }
 
-while getopts ':b:a:s:r:' arg; do
-	case "${arg}" in
-		b) BRANCH=$OPTARG ;;
-		a) ARCH=$OPTARG ;;
-		s) START_DIR=$OPTARG ;;
-		r) CHROOT=$OPTARG ;;
-		h|*) usage ;;
-	esac
+while getopts 'b:a:p:snxh' arg; do
+    case "${arg}" in
+	b) BRANCH=$OPTARG ;;
+	a) ARCH=$OPTARG ;;
+	p) PROFILE=$OPTARG ;;
+	s) SIGN=1 ;;
+	n) NOCLEAN='' ;;
+	x) NOVERIFY='' ;;
+	h|*) usage ;;
+    esac
 done
 shift $((OPTIND-1))
 
-mk_pkg $@
+WORKDIR=${CHROOT}/${BRANCH}-${ARCH}
 
-############################################################
-
-# BRANCH=${1:-unstable}
-# ARCH=${2:-$(uname -m)}
-# START_DIR=${3:-all} # options: openrc,eudev,all
-#
-# mk_pkg $1 $2 $3
-
-#############################################################
+run $@
